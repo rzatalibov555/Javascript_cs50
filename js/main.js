18fe52702d8f7e137169312230fb32e2ad08dcd5
rzatalibov555 <sethubpro@gmail.com>
2024-07-11 15:37:00 +0400

    vacr

---
+++ js/main.js
@@ -0,0 +1,110 @@
+'use strict'
+
+
+// var a = "Rza"   // string //  str
+// var b = 10      // Number // num // integer // int
+// var geldi = true // Boolean // bool // true = 1, false = 0
+// var b_i = 2.140521232017996e+20 //Biginteger // Bigint
+
+// var ad   // undefined
+
+// var t = null // Null
+
+// var obj = {}
+// var arr = []
+// var c = a + b
+
+
+
+
+// console.log(t)
+
+// console.log(c +" -> "+ typeof c)
+
+
+
+// JavaScript has 8 Datatypes
+// String
+// Number
+// Bigint
+// Boolean
+// Undefined
+// Null
+// Symbol
+// Object
+
+
+// var ad = "Ibrahim"
+// var Ad = "Murad"
+// var aD = "Nurcan"
+// var AD = "Xaliq"
+
+// var a_d = "Rza"
+// var soyad = "Talibov"
+
+// console.log(a_d + "      " + soyad)
+// console.log(`${a_d}      ${soyad}`)
+// document.write(`${a_d} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ${soyad}`)
+
+
+
+
+// var a, b, c, d = 5, e
+
+// a = 6
+
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(e)
+
+
+
+
+// var a = 5
+
+// console.log(a)
+
+// var a = 25
+
+// console.log(a)
+
+
+
+// var a = 5
+// console.log(a)
+
+
+// var a = 25
+// console.log(b)
+
+// const a = ["BMW"]
+
+
+
+// console.log(a)
+
+
+// a = {car:"Tesla"}
+
+
+// console.log(a)
+
+
+
+
+// let _muradin_deyiweni = new Date()
+
+
+// console.log(_muradin_deyiweni)
+
+// let a = (2 + "Xaliq"  + 2 + 1)
+// console.log(a)
+
+
+
+
+
+
